d9691035e646195dc0e71ad548a2db50b9108455
allentevyaw <atevyaw@gmail.com>
2023-01-18 15:39:56 -0500

    nav & footer

---
+++ pages/product/[slug].js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { client, useFor } from '@/Lib/client'
+
+const ProductDetails = () => {
+  return (
+    <div>
+        <div className='product-detail-container'>
+            <div>
+                <div className='image-container'>
+                    <img src='' />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProductDetails
