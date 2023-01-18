d28c7129c5f47c422c5dfb512dae2a5238199ddf
allentevyaw <atevyaw@gmail.com>
2023-01-18 14:45:43 -0500

    footer

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,8 +1,28 @@
+import Link from 'next/link'
 import React from 'react'
+import { urlFor } from '@/Lib/client'
 
-const FooterBanner = () => {
+const FooterBanner = ({footerBanner: {
+discount, largeText1, largeText2, saleTime, smallText,
+midText, product, buttonText, image, desc
+}}) => {
   return (
-    <div>FooterBanner</div>
+    <div className='footer-banner-container'>
+        <div className='banner-desc'>
+            <div className='left'>
+                <p>{discount}</p>
+                <h3>{largeText1}</h3>
+                <h3>{largeText2}</h3>
+                <p>{saleTime}</p>
+            </div>
+            <div className='right'>
+                <p>{smallText}</p>
+                <h3>{midText}</h3>
+                <p>{desc}</p>
+            </div>
+        </div>
+
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,16 @@ const Home = ({products, bannerData}) => {
     <>
       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       {console.log(bannerData)}
-      <div className='products-heading'>
-        <h2>Best Selling Products</h2>
-        <p>Speakers of many variations</p>
-      </div>
+        <div className='products-heading'>
+          <h2>Best Selling Products</h2>
+          <p>Speakers of many variations</p>
+        </div>
 
       <div className='products-container'>
         {products?.map((product) => <Product key={product._id} product={product}/>)}
       </div>
 
-    <FooterBanner />
+    <FooterBanner footerBanner={bannerData.length && bannerData[0]}/>
     </>
   )
 }
