b8e74a05043f2cb4084259688bfb77276724ef34
allentevyaw <atevyaw@gmail.com>
2023-01-17 20:46:34 -0500

    sanity setup

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import product from '@/sanity_ecommerce/schemas/product'
 import React from 'react'
-import {Product, FooterBanner, HeroBanner} from '../components'
+import { client } from '@/Lib/client'
+import {Product, FooterBanner, HeroBanner, Footer} from '../components'
 
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
@@ -15,9 +16,21 @@ const Home = () => {
         {['Product 1', 'Product 2'].map((product) => product)}
       </div>
 
-    Footer
+    <FooterBanner />
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_types == "product"]'
+  const products = await client.fetch(query)
+
+  const bannerQuery = '*[_types == "banner"]'
+  const bannerData = await client.fetch(bannerQuery)
+
+  return {
+    props: {products, bannerData}
+  }
 }
 
 export default Home
