5bae17defe5ba108a271376d5c66fdef4f69c860
allentevyaw <atevyaw@gmail.com>
2023-01-18 19:31:27 -0500

    product details

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,18 +1,78 @@
 import React from 'react'
-import { client, useFor } from '@/Lib/client'
+import { client, urlFor } from '@/Lib/client'
+import { AiOutlineMinus, AiOutlinePlus, 
+    AiFillStar, AiOutlineStar } from 'react-icons/ai'
 
-const ProductDetails = () => {
+const ProductDetails = ({product, products}) => {
+const {image, name, details, price} = product
+
+
   return (
     <div>
         <div className='product-detail-container'>
             <div>
                 <div className='image-container'>
-                    <img src='' />
+                    <img src={urlFor(image && image[0])} />
                 </div>
+                {/* <div className='small-images-container'>
+                    {image?.map((item, i) => (
+                        <img
+                            src={urlFor(item)}
+                            className=''
+                            onMouseEnter=''
+                        />
+                    ))}
+                </div> */}
+            </div>
+            <div className='product-details-desc'>
+                <h1>{name}</h1>
+                <div className='reviews'>
+                    <div>
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiOutlineStar />
+                    </div>
+                <p>(20)</p>
+                </div>
+                <h4>Details</h4>
+                <p>{details}</p>
             </div>
         </div>
     </div>
   )
 }
+
+export const getStaticPaths = async () => {
+    const query = `*[_type == "product"] {
+        slug {
+            current 
+        }
+    }`
+    const products = await client.fetch(query)
+    const paths = products.map((product) => ({
+        params: {
+            slug: product.slug.current
+        }
+    }))
+
+    return {
+        paths, 
+        fallback: 'blocking'
+    }
+}
+
+export const getStaticProps = async ({params: {slug}}) => {
+    const query = `*[_type == "product" && slug.current == '${slug}'][0]`
+    const product = await client.fetch(query)
+
+    const productsQuery = '*[_type == "product"]'
+    const products = await client.fetch(productsQuery)
+  
+    return {
+      props: {products, product}
+    }
+  }
 
 export default ProductDetails
