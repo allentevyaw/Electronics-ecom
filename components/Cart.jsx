375ad968cc19c0cb4b4598729107a9278cab3ca0
allentevyaw <atevyaw@gmail.com>
2023-01-19 19:13:57 -0500

    building out cart

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -14,7 +14,42 @@ const cartRef = useRef()
 const {totalPrice, totalQuantities, cartItems, setShowCart} =useStateContext()
 
   return (
-    <div>Cart</div>
+    <div className='cart-wrapper' ref={cartRef}>
+      <div className='cart-container'>
+        <button 
+        type='button'
+        className='cart-heading'
+        onClick={() => setShowCart(false)}>
+            <AiOutlineLeft />
+            <span className='heading'>Your Cart</span>
+            <span className='cart-num-items'>{totalQuantities} items</span>
+        </button>
+
+{cartItems.length < 1 && (
+  <div className='empty-cart'>
+    <AiOutlineShopping size={150} />
+    <h3>Your shoppoing bag is empty</h3>
+    <Link href='/'>
+      <button 
+        type='button'
+        onClick={() => setShowCart(false)}
+        className='btn'>
+          Continue Shopping
+        </button>
+    </Link>
+  </div>
+)}
+
+<div className='product-container'>
+  {cartItems.length >= 1 && cartItems.map((item, index) => (
+    <div className='product' key={item._id}>
+      <img src={urlFor(item?.image[0])}
+    </div>
+  ))}
+</div>
+
+      </div>
+    </div>
   )
 }
 
