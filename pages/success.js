8418356c99cef477ae93f527d2706673bb01ec57
allentevyaw <atevyaw@gmail.com>
2023-01-22 18:43:55 -0500

    order complete page start

--- pages/success.js
+++ pages/success.js
@@ -1,18 +1,23 @@
 import React, {useState, useEffect} from 'react'
 import Link from 'next/link'
 import {BsBagCheckFill} from 'react-icons/bs'
-import { useRouter } from 'next/router'
 import { useStateContext } from '@/context/StateContext'
 
 const Success = () => {
     const {setCartItems, setTotalPrice, setTotalQuantities} = useStateContext()
-    const [order, setOrder] = useState(null)
 
   return (
     <div className='success-wrapper'>
         <div className='success'>
             <p className='icon'>
                 <BsBagCheckFill />
+            </p>
+            <h2>Thank you for your order!</h2>
+            <p className='email-msg'>Check your email inbox for the recepit</p>
+            <p className='description'>If you have any questions
+                <a className='email' href=''>
+
+                </a>
             </p>
         </div>
     </div>
