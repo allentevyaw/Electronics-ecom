9d56b6993afe7243c814dc8c07b0a1fa1322acf2
allentevyaw <atevyaw@gmail.com>
2023-01-22 19:03:37 -0500

    added confetti to order complete page

--- pages/success.js
+++ pages/success.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react'
 import Link from 'next/link'
 import {BsBagCheckFill} from 'react-icons/bs'
 import { useStateContext } from '@/context/StateContext'
+import { runFireworks } from '@/Lib/utils'
+
 
 const Success = () => {
     const {setCartItems, setTotalPrice, settotalQuantities} = useStateContext()
@@ -11,6 +13,7 @@ const Success = () => {
         setCartItems([])
         setTotalPrice(0)
         settotalQuantities(0)
+        runFireworks()
     }, [])
 
   return (
